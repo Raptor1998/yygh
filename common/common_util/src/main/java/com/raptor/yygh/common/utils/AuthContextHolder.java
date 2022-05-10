4038ad89f0ba6e3a61ba224e2c429e2df59cf50b
Raptor1998 <knightcwh@163.com>
2022-05-10 15:38:42 +0800

    通用模块

---
+++ common/common_util/src/main/java/com/raptor/yygh/common/utils/AuthContextHolder.java
@@ -0,0 +1,26 @@
+package com.raptor.yygh.common.utils;
+
+
+import com.raptor.yygh.common.helper.JwtHelper;
+
+import javax.servlet.http.HttpServletRequest;
+
+//获取当前用户信息的工具类
+public class AuthContextHolder {
+    //获取当前用户id
+    public static Long getUserId(HttpServletRequest request){
+        //从header中获取token
+        String token = request.getHeader("token");
+        //从token中获取userid
+        Long userId = JwtHelper.getUserId(token);
+        return userId;
+    }
+    //获取当前用户名称
+    public static String getUserName(HttpServletRequest request){
+        //从header中获取token
+        String token = request.getHeader("token");
+        //从token中获取userid
+        String userName = JwtHelper.getUserName(token);
+        return userName;
+    }
+}
