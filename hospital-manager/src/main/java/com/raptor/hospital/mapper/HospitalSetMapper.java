de7780f5a280955e4282fd2e1e56f4111fe16664
Raptor1998 <knightcwh@163.com>
2022-05-10 15:41:16 +0800

    模拟医院方接口

---
+++ hospital-manager/src/main/java/com/raptor/hospital/mapper/HospitalSetMapper.java
@@ -0,0 +1,12 @@
+package com.raptor.hospital.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.raptor.hospital.model.HospitalSet;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
---
+++ hospital-manager/src/main/java/com/raptor/hospital/mapper/OrderInfoMapper.java
@@ -0,0 +1,11 @@
+package com.raptor.hospital.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.raptor.hospital.model.OrderInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
+
+}
